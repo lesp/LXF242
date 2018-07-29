a3e09f754c0d45b2b792d682ad0099eeb22abd1b
Les Pounder <lespounder@gmail.com>
2018-07-29 16:25:06 +0100

    Working code

--- bookscanner.py
+++ bookscanner.py
@@ -1,10 +1,12 @@
 import isbnlib
-from guizero import App, Text, PushButton, TextBox
+from guizero import App, Text, PushButton, TextBox, Window
 import webbrowser
 global ISBN13
+global app
 
 def update_details():
     global ISBN13
+    global app
     book = input_box.value
     book_meta = isbnlib.meta(book)
     description = isbnlib.desc(book)
@@ -14,12 +16,13 @@ def update_details():
     author = book_meta["Authors"][0]
     year = book_meta["Year"]
     ISBN13 = book_meta["ISBN-13"]
-    Publication_Title = Text(app, text="Book Title: "+title, grid=[0,1], align="left")
-    Author_Details = Text(app, text="Author(s): "+author, grid=[0,2], align="left")
-    Publication_Year = Text(app, text="Year of publication: "+year, grid=[0,3], align="left")
-    ISBN13Data = Text(app, text="ISBN-13: "+ISBN13, grid=[0,4], align="left")
-    Description = Text(app, text="Description: "+description, grid=[0,5,1,5], align="left")
-    Visit_Amazon = PushButton(app, text="Visit Amazon", command=openAmazon,grid=[0,8], align="left")
+    details = Window(app,title=title+" "+author, width=700, layout="grid")
+    Publication_Title = Text(details, text="Book Title: "+title, grid=[0,1], align="left")
+    Author_Details = Text(details, text="Author(s): "+author, grid=[0,2], align="left")
+    Publication_Year = Text(details, text="Year of publication: "+year, grid=[0,3], align="left")
+    ISBN13Data = Text(details, text="ISBN-13: "+ISBN13, grid=[0,4], align="left")
+    Description = Text(details, text="Description: "+description, grid=[0,5], align="left")
+    Visit_Amazon = PushButton(details, text="Visit Amazon", command=openAmazon,grid=[0,8], align="left")
     return ISBN13
 
 def openAmazon():
@@ -27,7 +30,7 @@ def openAmazon():
     URL = "https://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords="
     webbrowser.open_new_tab(URL+ISBN13)
     
-app = App(title="Book Scanner", layout="grid", width=700)
+app = App(title="Book Scanner", layout="grid", width=700, height=200)
 
 info = Text(app, text="Scan or type the ISBN 13 digit code to search", enabled=True, grid =[0,0])
 input_box = TextBox(app, grid=[1,0])
